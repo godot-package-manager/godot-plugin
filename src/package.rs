e7cae5ad9088652813ec378f64742f69861b36d9
bendn <bend.n@outlook.com>
2022-12-16 07:27:50 +0700

    remove serde custom deserialization

--- src/package.rs
+++ src/package.rs
@@ -1,7 +1,6 @@
 use crate::npm::*;
 use flate2::read::GzDecoder;
 use regex::{Captures, Regex};
-use serde::de::{self, Deserializer, MapAccess, SeqAccess, Visitor};
 use serde::Deserialize;
 use std::fs::{create_dir_all, read_dir, read_to_string, remove_dir_all, write};
 use std::io;
@@ -296,100 +295,5 @@ impl fmt::Display for Package {
 impl fmt::Debug for Package {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.to_string())
-    }
-}
-
-impl<'de> Deserialize<'de> for Package {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        enum Field {
-            Name,
-            Version,
-        }
-
-        impl<'de> Deserialize<'de> for Field {
-            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                struct FieldVisitor;
-
-                impl<'de> Visitor<'de> for FieldVisitor {
-                    type Value = Field;
-
-                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                        formatter.write_str("`name` or `version`")
-                    }
-
-                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
-                    where
-                        E: de::Error,
-                    {
-                        match value {
-                            "name" => Ok(Field::Name),
-                            "version" => Ok(Field::Version),
-                            _ => Err(de::Error::unknown_field(value, FIELDS)),
-                        }
-                    }
-                }
-
-                deserializer.deserialize_identifier(FieldVisitor)
-            }
-        }
-
-        struct PackageVisitor;
-
-        impl<'de> Visitor<'de> for PackageVisitor {
-            type Value = Package;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("struct Package")
-            }
-
-            fn visit_seq<V>(self, mut seq: V) -> Result<Package, V::Error>
-            where
-                V: SeqAccess<'de>,
-            {
-                let name = seq
-                    .next_element()?
-                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
-                let version = seq
-                    .next_element()?
-                    .ok_or_else(|| de::Error::invalid_length(1, &self))?;
-                Ok(Package::new(name, version))
-            }
-
-            fn visit_map<V>(self, mut map: V) -> Result<Package, V::Error>
-            where
-                V: MapAccess<'de>,
-            {
-                let mut name = None;
-                let mut version = None;
-                while let Some(key) = map.next_key()? {
-                    match key {
-                        Field::Name => {
-                            if name.is_some() {
-                                return Err(de::Error::duplicate_field("name"));
-                            }
-                            name = Some(map.next_value()?);
-                        }
-                        Field::Version => {
-                            if version.is_some() {
-                                return Err(de::Error::duplicate_field("version"));
-                            }
-                            version = Some(map.next_value()?);
-                        }
-                    }
-                }
-                let name = name.ok_or_else(|| de::Error::missing_field("name"))?;
-                let version = version.ok_or_else(|| de::Error::missing_field("version"))?;
-                Ok(Package::new(name, version))
-            }
-        }
-
-        const FIELDS: &'static [&'static str] = &["name", "version"];
-        deserializer.deserialize_struct("Package", FIELDS, PackageVisitor)
     }
 }
